9ca8b339fc052e15e58bbb275f5c73e42cc58ab3
Ilia Bayanov <i.bajanov@itorum.ru>
2023-07-01 03:31:55 +0300

    refactor: rename argument, feat: pass responseFromWorker statuscode and headers to main response feat: handle errors

--- src/handlers/balancerRequestHandler.ts
+++ src/handlers/balancerRequestHandler.ts
@@ -3,7 +3,7 @@ import { IncomingMessage, ServerResponse, RequestOptions, request as serverReque
 let i = 0;
 
 export function balancerRequestHandler(
-  workers: number[],
+  workerPorts: number[],
   request: IncomingMessage,
   response: ServerResponse
 ) {
@@ -20,19 +20,27 @@ export function balancerRequestHandler(
 
   const requestOptions: RequestOptions = {
     path: request.url,
-    port: workers[i],
+    port: workerPorts[i],
     method: request.method,
   };
   const requestToWorker = serverRequest(
     'http://localhost',
     requestOptions,
     (responseFromWorker) => {
+      response.statusCode = responseFromWorker.statusCode!;
+      for (let header in responseFromWorker.headers) {
+        response.setHeader(header, responseFromWorker.headers[header]!);
+      }
       responseFromWorker.pipe(response);
     }
   );
-  request.pipe(requestToWorker);
+
+  request.pipe(requestToWorker).on('error', () => {
+    response.statusCode = 500;
+    response.end('<h1>Error 500 - Internal server error</h1>');
+  });
 
   i++;
 
-  if (i === workers.length) i = 0;
+  if (i === workerPorts.length) i = 0;
 }
